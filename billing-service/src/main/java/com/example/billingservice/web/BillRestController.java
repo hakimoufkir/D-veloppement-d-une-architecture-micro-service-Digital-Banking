fa4e36c1c7675dd6bf9bb97d75f937c3ff1d6976
Abdelhakim OUFKIR <abdelhakimoufkir2000@gmail.com>
2024-11-17 13:56:13 +0100

    adding billing service and fixing customer id problem

---
+++ billing-service/src/main/java/com/example/billingservice/web/BillRestController.java
@@ -0,0 +1,41 @@
+package com.example.billingservice.web;
+
+import com.example.billingservice.entities.Bill;
+import com.example.billingservice.entities.ProductItem;
+import com.example.billingservice.feign.CustomerRestClient;
+import com.example.billingservice.feign.ProductRestClient;
+import com.example.billingservice.model.Product;
+import com.example.billingservice.repository.BillRepository;
+import com.example.billingservice.repository.ProductItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BillRestController {
+    @Autowired
+    private BillRepository billRepository;
+    @Autowired
+    private ProductItemRepository productItemRepository;
+    @Autowired
+    private CustomerRestClient customerRestClient;
+    @Autowired
+    private ProductRestClient productRestClient;
+    @GetMapping(path = "/bills/{id}")
+    public Bill getBill(@PathVariable Long id){
+        Bill bill = billRepository.findById(id).get();
+        bill.setCustomer(customerRestClient.getCustomerById(bill.getCustomerId()));
+        bill.getProductItems().forEach(productItem -> {
+            Product product = null;
+            try {
+                product = productRestClient.getProductById(productItem.getProductId());
+            } catch (Exception e) {
+                System.err.println("Error fetching product: " + e.getMessage());
+                e.printStackTrace(); // Optional: prints the stack trace for debugging
+            }
+            productItem.setProduct(product);
+        });
+        return bill;
+    }
+}
